70d8a7a854babded35752aaa59f85931e471eb4f
zhilu-tang <zhilu.tang@gmail.com>
2024-11-26 22:11:20 +0800

    load testing

--- src/test/java/com/pkg/balance/mgmt/performance/JMeterLiveTest.java
+++ src/test/java/com/pkg/balance/mgmt/performance/JMeterLiveTest.java
@@ -1,18 +1,27 @@
 package com.pkg.balance.mgmt.performance;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
 
 import com.pkg.balance.mgmt.BalanceMgmtApplication;
+import com.pkg.balance.mgmt.entity.Account;
+import com.pkg.balance.mgmt.entity.Transaction;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.control.gui.LoopControlPanel;
 import org.apache.jmeter.control.gui.TestPlanGui;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.protocol.http.control.Header;
 import org.apache.jmeter.protocol.http.control.HeaderManager;
+import org.apache.jmeter.protocol.http.gui.HeaderPanel;
 import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
 import org.apache.jmeter.protocol.http.util.HTTPArgument;
 import org.apache.jmeter.reporters.ResultCollector;
@@ -20,19 +29,26 @@ import org.apache.jmeter.reporters.Summariser;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
+import org.apache.jmeter.testelement.property.ObjectProperty;
+import org.apache.jmeter.threads.SetupThreadGroup;
 import org.apache.jmeter.threads.ThreadGroup;
 import org.apache.jmeter.control.LoopController;
 import org.apache.jmeter.threads.gui.ThreadGroupGui;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 import org.apache.jorphan.collections.ListedHashTree;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import static org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase.HEADER_MANAGER;
+import static org.apache.jmeter.protocol.http.util.HTTPConstantsInterface.HEADER_CONTENT_TYPE;
+
 public class JMeterLiveTest {
 
     private static ConfigurableApplicationContext context;
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeAll
     public static void setUp() {
@@ -76,6 +92,9 @@ public class JMeterLiveTest {
         LoopController mainLoopController = getLoopController(1);
         ThreadGroup mainThreadGroup = getThreadGroup(mainLoopController, "Thread Group", 10, 5);
 
+        // Create Header Manager
+        HeaderManager manager = getHeaderManager();
+
         // Test Plan
         TestPlan testPlan = getTestPlan(setupThreadGroup, mainThreadGroup);
 
@@ -83,14 +102,24 @@ public class JMeterLiveTest {
         ListedHashTree testPlanTree = new ListedHashTree();
         testPlanTree.add(testPlan);
 
+        // HTTP Request Sampler and Header Manager
+        HashTree createAccountTree1 = new HashTree();
+        createAccountTree1.add(createAccount1, manager);
+        HashTree createAccountTree2 = new HashTree();
+        createAccountTree2.add(createAccount2, manager);
+
         // Add Setup Thread Group
         HashTree setupThreadGroupHashTree = testPlanTree.add(testPlan, setupThreadGroup);
-        setupThreadGroupHashTree.add(createAccount1);
-        setupThreadGroupHashTree.add(createAccount2);
+        setupThreadGroupHashTree.add(createAccountTree1);
+        setupThreadGroupHashTree.add(createAccountTree2);
+
+        // HTTP Request Sampler and Header Manager
+        HashTree createTransactionTree = new HashTree();
+        createTransactionTree.add(createTransaction, manager);
 
         // Add Main Thread Group
         HashTree mainThreadGroupHashTree = testPlanTree.add(testPlan, mainThreadGroup);
-        mainThreadGroupHashTree.add(createTransaction);
+        mainThreadGroupHashTree.add(createTransactionTree);
 
         // Save the test plan to a .jmx file
         SaveService.saveTree(testPlanTree, Files.newOutputStream(Paths.get("target", "script.jmx")));
@@ -108,6 +137,17 @@ public class JMeterLiveTest {
 
         System.out.println("Test completed. See output-logs.jtl file for results");
         System.out.println("JMeter .jmx script is available at script.jmx");
+    }
+
+    private static @NotNull HeaderManager getHeaderManager() {
+        // https://stackoverflow.com/questions/41605836/jmeter-add-custom-http-headers-programatically-in-httpsampler
+        HeaderManager manager = new HeaderManager();
+        manager.add(new Header(HEADER_CONTENT_TYPE, "application/json;"));
+        manager.add(new Header("Accept", "application/json"));
+        manager.setName(JMeterUtils.getResString("header_manager_title")); // $NON-NLS-1$
+        manager.setProperty(TestElement.TEST_CLASS, HeaderManager.class.getName());
+        manager.setProperty(TestElement.GUI_CLASS, HeaderPanel.class.getName());
+        return manager;
     }
 
     private static TestPlan getTestPlan(ThreadGroup... threadGroups) {
@@ -141,51 +181,51 @@ public class JMeterLiveTest {
         return loopController;
     }
 
-    private static HTTPSamplerProxy getCreateAccountSampler(long accountNumber, long balance) {
+    private static HTTPSamplerProxy getCreateAccountSampler(long accountNumber, long balance) throws JsonProcessingException {
         HTTPSamplerProxy httpSampler = new HTTPSamplerProxy();
         httpSampler.setDomain("localhost");
         httpSampler.setPort(8080);
         httpSampler.setPath("/api/account/create");
         httpSampler.setMethod("POST");
         httpSampler.setPostBodyRaw(true);
+
+        Account account = new Account();
+        account.setAccountNumber(String.valueOf(accountNumber));
+        account.setBalance(balance);
+
         Arguments arguments = new Arguments();
-        HTTPArgument bodyArg = new HTTPArgument("", "{\n" +
-                "  \"accountNumber\": " + accountNumber + ",\n" +
-                "  \"balance\": " + balance + "\n" +
-                "}", "", false);
+        HTTPArgument bodyArg = new HTTPArgument("", objectMapper.writeValueAsString(account), null, true);
+        bodyArg.setAlwaysEncoded(false);
         arguments.addArgument(bodyArg);
         httpSampler.setArguments(arguments);
 
-        // Add Header Manager
-        HeaderManager headerManager = new HeaderManager();
-        headerManager.add(new Header("content-type", "application/json"));
-        headerManager.add(new Header("accept", "application/json"));
-        httpSampler.addTestElement(headerManager);
+        // 打印请求体
+        System.out.println("Request Body: " + bodyArg.getValue());
 
         return httpSampler;
     }
 
-    private static HTTPSamplerProxy getCreateTransactionSampler(long sourceAccountNumber, long destinationAccountNumber, double amount) {
+    private static HTTPSamplerProxy getCreateTransactionSampler(long sourceAccountNumber, long destinationAccountNumber, double amount) throws JsonProcessingException {
         HTTPSamplerProxy httpSampler = new HTTPSamplerProxy();
         httpSampler.setDomain("localhost");
         httpSampler.setPort(8080);
         httpSampler.setPath("/api/account/createTransaction");
         httpSampler.setMethod("POST");
         httpSampler.setPostBodyRaw(true);
+
+        Transaction transaction = new Transaction();
+        transaction.setSourceAccountNumber(String.valueOf(sourceAccountNumber));
+        transaction.setDestinationAccountNumber(String.valueOf(destinationAccountNumber));
+        transaction.setAmount(amount);
+
         Arguments arguments = new Arguments();
-        HTTPArgument bodyArg = new HTTPArgument("", "{\n" +
-                "  \"sourceAccountNumber\": " + sourceAccountNumber + ",\n" +
-                "  \"destinationAccountNumber\": " + destinationAccountNumber + ",\n" +
-                "  \"amount\": " + amount + "\n" +
-                "}", "", false);
+        HTTPArgument bodyArg = new HTTPArgument("", objectMapper.writeValueAsString(transaction), null, true);
+        bodyArg.setAlwaysEncoded(false);
         arguments.addArgument(bodyArg);
         httpSampler.setArguments(arguments);
 
-        // Add Header Manager
-        HeaderManager headerManager = new HeaderManager();
-        headerManager.add(new Header("content-type", "application/json"));
-        headerManager.add(new Header("accept", "application/json"));
-        httpSampler.addTestElement(headerManager);
+        // 打印请求体
+        System.out.println("Request Body: " + bodyArg.getValue());
 
         return httpSampler;
     }
